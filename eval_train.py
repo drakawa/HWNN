63f515899abb5967bc88201e56a8925ff97f4021
Ryuta Kawano <ryuuta.kawano@gmail.com>
2022-04-22 14:21:32 +0000

    delete irreative line

--- eval_train.py
+++ eval_train.py
@@ -5,5 +5,3 @@ gs = ["rrg","symsa","ws"]
 for g in gs:
     for s in ss:
         print("python main_cifar10.py -n rwnn -g {} -s {} -m train".format(g, s))
-
-print("python main_cifar10.py -n rwnn -g {} -s {} -m train".format(""))
