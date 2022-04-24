220d51a5df423ab8fecc1884ff11082c96c4392f
Ryuta Kawano <ryuuta.kawano@gmail.com>
2022-04-25 00:57:12 +0900

    save edgefiles

--- gen_graph.py
+++ gen_graph.py
@@ -1,6 +1,8 @@
 import argparse
 import networkx as nx
 import numpy as np
+import os
+import pandas as pd
 
 class GConfig:
     def __init__(self, d: int = None, n: int = None, s: int = None, p: float = None, g: int = None, m: int = None, name: str = None):
@@ -22,33 +24,47 @@ class GenGs:
             randstate = np.random.RandomState(self.config.s)
 
         if self.config.name == "2dtorus":
-            G = nx.grid_graph([self.config.n, self.config.m], periodic=True)
-            G_nodes = sorted(list(G.nodes()))
-            mapping = {node: G_nodes.index(node) for node in G_nodes}
-
-            G = nx.relabel_nodes(G, mapping)
-            Gs = [G.copy() for _ in range(self.num_graphs)]
             name = f"2dtorus_{self.config.m}_{self.config.m}"
-
         elif self.config.name == "rrg":
-            Gs = [nx.random_regular_graph(self.config.d, self.config.n, randstate) for _ in range(self.num_graphs)]
             name = f"rrg_{self.config.d}_{self.config.n}_{self.config.s}"
-
         elif self.config.name == "ws":
-            Gs = [nx.connected_watts_strogatz_graph(self.config.n, self.config.d, self.config.p, seed=randstate) for _ in range(self.num_graphs)]
             name = f"ws_{self.config.n}_{self.config.d}_{self.config.p}_{self.config.s}"
-
         elif self.config.name == "symsa":
-            symsa_seeds = [(self.config.s - 1) * self.num_graphs + offset + 1 for offset in range(self.num_graphs)]
-            edgelists = ["symsa_edgefiles/symsa_s{}_{}_{}_{}.txt".format(self.config.g, self.config.n, self.config.d, i) for i in symsa_seeds]
-            print("edgelists:", edgelists)
-            Gs = [nx.read_edgelist(edgelist, nodetype=int) for edgelist in edgelists]
-            # print(Gs[0].edges())
             name = f"symsa_{self.config.n}_{self.config.d}_{self.config.g}_{self.config.s}"
-
         else:
             print("Invalid config:", self.config)
             exit(1)
+            
+        graph_path = os.path.join("./graphs", name + ".edges")
+        if os.path.exists(graph_path):
+            Gs = pd.read_pickle(graph_path)
+        else:
+            if self.config.name == "2dtorus":
+                G = nx.grid_graph([self.config.n, self.config.m], periodic=True)
+                G_nodes = sorted(list(G.nodes()))
+                mapping = {node: G_nodes.index(node) for node in G_nodes}
+
+                G = nx.relabel_nodes(G, mapping)
+                Gs = [G.copy() for _ in range(self.num_graphs)]
+
+            elif self.config.name == "rrg":
+                Gs = [nx.random_regular_graph(self.config.d, self.config.n, randstate) for _ in range(self.num_graphs)]
+
+            elif self.config.name == "ws":
+                Gs = [nx.connected_watts_strogatz_graph(self.config.n, self.config.d, self.config.p, seed=randstate) for _ in range(self.num_graphs)]
+
+            elif self.config.name == "symsa":
+                symsa_seeds = [(self.config.s - 1) * self.num_graphs + offset + 1 for offset in range(self.num_graphs)]
+                edgelists = ["symsa_edgefiles/symsa_s{}_{}_{}_{}.txt".format(self.config.g, self.config.n, self.config.d, i) for i in symsa_seeds]
+                print("edgelists:", edgelists)
+                Gs = [nx.read_edgelist(edgelist, nodetype=int) for edgelist in edgelists]
+                # print(Gs[0].edges())
+
+            else:
+                print("Invalid config:", self.config)
+                exit(1)
+
+            pd.to_pickle(Gs, graph_path)
 
         return Gs, name
 
@@ -82,4 +98,3 @@ if __name__ == "__main__":
     Gs, name = gen_Gs.gen_Gs()
     print(Gs, name)
     print(Gs[0].edges())
-
