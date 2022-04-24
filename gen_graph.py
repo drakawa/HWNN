4ee60b0f8e03be394654ca2c65d9ac7d83ba633d
Ryuta Kawano <ryuuta.kawano@gmail.com>
2022-04-25 07:16:20 +0900

    reorder method added, set default seed

--- gen_graph.py
+++ gen_graph.py
@@ -4,8 +4,33 @@ import numpy as np
 import os
 import pandas as pd
 
+def reorder_G(G, method, rev=False):
+    print("reorder_G:", G.nodes(), G.edges())
+    center = np.random.choice(nx.center(G))
+    G_nodes = sorted(list(G.nodes()))
+
+    if method == "random":
+        new_G_nodes = list(G_nodes)
+        np.random.shuffle(new_G_nodes)
+    elif method == "bfs":
+        edges = nx.bfs_edges(G, center)
+        new_G_nodes = [center] + [v for u, v in edges]
+    elif method == "dfs":
+        edges = nx.dfs_edges(G, center)
+        new_G_nodes = [center] + [v for u, v in edges]
+    else:
+        print("invalid method:", method)
+        exit(1)
+
+    if method in ["bfs", "dfs"] and rev:
+        new_G_nodes.reverse()
+
+    mapping = {i:j for i,j in zip(G_nodes, new_G_nodes)}
+    newG = nx.relabel_nodes(G, mapping)
+    return newG
+
 class GConfig:
-    def __init__(self, d: int = None, n: int = None, s: int = None, p: float = None, g: int = None, m: int = None, name: str = None):
+    def __init__(self, d: int = None, n: int = None, s: int = 0, p: float = None, g: int = None, m: int = None, name: str = None, method: str = None, rev: bool = False):
         self.d: int = d
         self.n: int = n
         self.s: int = s
@@ -13,6 +38,8 @@ class GConfig:
         self.g: int = g
         self.m: int = m
         self.name: str = name
+        self.method: str = method
+        self.rev: bool = rev
 
 class GenGs:
     def __init__(self, config: GConfig):
@@ -34,6 +61,14 @@ class GenGs:
         else:
             print("Invalid config:", self.config)
             exit(1)
+
+        if self.config.method == "random":
+            name += "_random"
+        elif self.config.method in ["bfs", "dfs"]:
+            if not self.config.rev:
+                name += "_" + self.config.method
+            else:
+                name += "_" + self.config.method + "_" + "rev"
             
         graph_path = os.path.join("./graphs", name + ".edges")
         if os.path.exists(graph_path):
@@ -63,6 +98,9 @@ class GenGs:
             else:
                 print("Invalid config:", self.config)
                 exit(1)
+
+            if self.config.method:
+                Gs = [reorder_G(G, self.config.method, self.config.rev) for G in Gs]
 
             pd.to_pickle(Gs, graph_path)
 
@@ -98,3 +136,22 @@ if __name__ == "__main__":
     Gs, name = gen_Gs.gen_Gs()
     print(Gs, name)
     print(Gs[0].edges())
+
+    g_config = GConfig(n=32,d=4,g=4,s=3,name="symsa",method="dfs")
+    gen_Gs = GenGs(g_config)
+    Gs, name = gen_Gs.gen_Gs()
+    print(Gs, name)
+    print(Gs[0].edges())
+
+    g_config = GConfig(n=32,d=4,g=4,s=3,name="symsa",method="random")
+    gen_Gs = GenGs(g_config)
+    Gs, name = gen_Gs.gen_Gs()
+    print(Gs, name)
+    print(Gs[0].edges())
+
+    g_config = GConfig(n=32,d=4,g=4,s=3,name="symsa",method="bfs",rev=True)
+    gen_Gs = GenGs(g_config)
+    Gs, name = gen_Gs.gen_Gs()
+    print(Gs, name)
+    print(Gs[0].edges())
+
