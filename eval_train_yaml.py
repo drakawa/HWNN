d480eb05f76768517395d9c82af590fe00888de3
Ryuta Kawano <ryuuta.kawano@gmail.com>
2022-04-25 15:18:05 +0900

    eval script

---
+++ eval_train_yaml.py
@@ -0,0 +1,44 @@
+import yaml
+import sys
+import argparse
+import argcomplete
+import itertools as it
+
+def load_yaml(yaml_path):
+    try:
+        with open(yaml_path) as f:
+            obj = yaml.safe_load(f)
+            # print(obj)
+    except Exception as e:
+        print(e)
+        exit(1)
+
+    return obj
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='RWNN evaluation from yaml file')
+    parser.add_argument('-y', '--yaml_path', type=str, help='yaml filepath')
+
+    argcomplete.autocomplete(parser)
+
+    args = parser.parse_args()
+    # print(args)
+    yaml_path = args.yaml_path
+
+    yaml_obj = load_yaml(yaml_path)
+    ns, gs, ss, rm_revs = yaml_obj["nets"],  yaml_obj["graphs"],  yaml_obj["seeds"],  yaml_obj["reorder_method_revs"]
+
+    # print(ns, gs, ss, rm_revs)
+
+    for n, g, (r, is_rev), s in it.product(ns, gs, rm_revs, ss):
+        if is_rev:
+            rev_str = " --rev"
+        else:
+            rev_str = ""
+        print("python main_cifar10.py -n {} -g {} -s {} -r {}{} -m train".format(n, g, s, r, rev_str))
+    
+# ss = list(range(1,11))
+# gs = ["rrg","symsa","ws"]
+
+# for g in gs:
+#     for s in ss:
+#         print("python main_cifar10.py -n rwnn -g {} -s {} -m train".format(g, s))
