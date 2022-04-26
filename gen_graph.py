6defed16d6031649fb66274dfde0f31a5d84d187
Ryuta Kawano <ryuuta.kawano@gmail.com>
2022-04-27 07:24:29 +0900

    gen_bipartiteG, use randomstate, correct rrg name

--- gen_graph.py
+++ gen_graph.py
@@ -4,15 +4,54 @@ import numpy as np
 import os
 import pandas as pd
 
-def reorder_G(G, method, rev=False):
+def gen_bipartiteG(n, d, rs):
+    if n % 2 != 0:
+        print("n should be even number:", n)
+        exit(1)
+    
+    rights = [set() for _ in range(n//2)]
+    tmp_d = 0
+    max_retry = 100
+    num_retry = 0
+    while tmp_d < d:
+        tmp_rights = [set(s) for s in rights]
+
+        cands = [set(range(n//2)) - rights[i] for i in range(n//2)]
+ 
+        for i in range(n//2):
+            if len(cands[i]) == 0:
+                num_retry += 1
+                if num_retry >= max_retry:
+                    print("too many retry.")
+                    exit(1)
+                break
+            j = rs.choice(list(cands[i]))
+            tmp_rights[i].add(j)
+            cands = [cand - set([j]) for cand in cands]
+
+        else:
+            rights = tmp_rights
+            tmp_d += 1
+
+    print(rights)
+
+    G = nx.Graph()
+    G.add_nodes_from(range(n))
+    for i in range(n//2):
+        for j in rights[i]:
+            G.add_edge(i,j+n//2)
+    return G
+
+def reorder_G(G, method, rs, rev=False):
     # print("reorder_G:", G.nodes(), G.edges())
-    center = np.random.choice(nx.barycenter(G))
+    # center = np.random.choice(nx.barycenter(G))
+    center = rs.choice(nx.barycenter(G))
     print("centers:", nx.barycenter(G))
     G_nodes = sorted(list(G.nodes()))
 
     if method == "random":
         new_G_nodes = list(G_nodes)
-        np.random.shuffle(new_G_nodes)
+        rs.shuffle(new_G_nodes)
     elif method == "bfs":
         edges = nx.bfs_edges(G, center)
         new_G_nodes = [center] + [v for u, v in edges]
@@ -54,11 +93,13 @@ class GenGs:
         if self.config.name == "2dtorus":
             name = f"2dtorus_{self.config.n}_{self.config.m}"
         elif self.config.name == "rrg":
-            name = f"rrg_{self.config.d}_{self.config.n}_{self.config.s}"
+            name = f"rrg_{self.config.n}_{self.config.d}_{self.config.s}"
         elif self.config.name == "ws":
             name = f"ws_{self.config.n}_{self.config.d}_{self.config.p}_{self.config.s}"
         elif self.config.name == "symsa":
             name = f"symsa_{self.config.n}_{self.config.d}_{self.config.g}_{self.config.s}"
+        elif self.config.name == "bipartite":
+            name = f"bipartite_{self.config.n}_{self.config.d}_{self.config.s}"
         else:
             print("Invalid config:", self.config)
             exit(1)
@@ -96,13 +137,16 @@ class GenGs:
                 Gs = [nx.read_edgelist(edgelist, nodetype=int) for edgelist in edgelists]
                 # print(Gs[0].edges())
 
+            elif self.config.name == "bipartite":
+                Gs = [gen_bipartiteG(self.config.n, self.config.d, randstate) for _ in range(self.num_graphs)]
+
             else:
                 print("Invalid config:", self.config)
                 exit(1)
 
             if self.config.method:
                 print("call_method")
-                Gs = [reorder_G(G, self.config.method, self.config.rev) for G in Gs]
+                Gs = [reorder_G(G, self.config.method, randstate, self.config.rev) for G in Gs]
 
             pd.to_pickle(Gs, graph_path)
 
@@ -162,3 +206,13 @@ if __name__ == "__main__":
     Gs, name = gen_Gs.gen_Gs()
     print(Gs, name)
     print(Gs[0].edges())
+
+    g_config = GConfig(n=32,d=4,s=3,name="bipartite")
+    gen_Gs = GenGs(g_config)
+    Gs, name = gen_Gs.gen_Gs()
+    print(Gs, name)
+    print(Gs[0].edges())
+
+    # G = gen_bipartiteG(32, 4, state=None)
+    # print(G.edges())
+    # print(G.degree())
