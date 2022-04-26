d0f5341ef9c270b8570d9a1cce958fad23611a75
Ryuta Kawano <ryuuta.kawano@gmail.com>
2022-04-26 18:34:24 +0900

    yaml evaluation for non-random

--- eval_train_yaml.py
+++ eval_train_yaml.py
@@ -16,7 +16,7 @@ def load_yaml(yaml_path):
     return obj
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='RWNN evaluation from yaml file')
-    parser.add_argument('-y', '--yaml_path', type=str, help='yaml filepath')
+    parser.add_argument('yaml_path', type=str, help='yaml filepath')
 
     argcomplete.autocomplete(parser)
 
@@ -25,16 +25,36 @@ if __name__ == "__main__":
     yaml_path = args.yaml_path
 
     yaml_obj = load_yaml(yaml_path)
-    ns, gs, ss, rm_revs = yaml_obj["nets"],  yaml_obj["graphs"],  yaml_obj["seeds"],  yaml_obj["reorder_method_revs"]
+    ns = yaml_obj.get("nets")
+    gs = yaml_obj.get("graphs", [None])
+    ss = yaml_obj.get("seeds", [None])
+    rm_revs = yaml_obj.get("reorder_method_revs", [(None, None)])
+    # ns, gs, ss, rm_revs = yaml_obj["nets"],  yaml_obj["graphs"],  yaml_obj["seeds"],  yaml_obj["reorder_method_revs"]
 
     # print(ns, gs, ss, rm_revs)
 
     for n, g, (r, is_rev), s in it.product(ns, gs, rm_revs, ss):
-        if is_rev:
-            rev_str = " --rev"
-        else:
-            rev_str = ""
-        print("python main_cifar10.py -n {} -g {} -s {} -r {}{} -m train".format(n, g, s, r, rev_str))
+        args_str = ""
+        if n:
+            args_str += "-n {} ".format(n)
+        if g:
+            args_str += "-g {} ".format(g)
+        if s:
+            args_str += "-s {} ".format(s)
+        if r:
+            if is_rev:
+                rev_str = " --rev"
+            else:
+                rev_str = ""
+            args_str += "-r {}{} ".format(r, rev_str)
+        print("python main_cifar10.py {}-m train".format(args_str))
+
+    # for n, g, (r, is_rev), s in it.product(ns, gs, rm_revs, ss):
+    #     if is_rev:
+    #         rev_str = " --rev"
+    #     else:
+    #         rev_str = ""
+    #     print("python main_cifar10.py -n {} -g {} -s {} -r {}{} -m train".format(n, g, s, r, rev_str))
     
 # ss = list(range(1,11))
 # gs = ["rrg","symsa","ws"]
