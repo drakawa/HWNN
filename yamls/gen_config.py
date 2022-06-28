647b18f057211bef9ef1cf13ccc388a79c70acc6
Ryuta Kawano <ryuuta.kawano@gmail.com>
2022-06-28 15:41:36 +0900

    symsa eval

--- yamls/gen_config.py
+++ yamls/gen_config.py
@@ -5,12 +5,14 @@ import os
 if __name__ == "__main__":
         
     ns = ["rwnn"]
-    gs = ["ws"]
+    # gs = ["ws"]
+    gs = ["symsa"]
     ss = list(range(1,11))
-    rm_revs = [("random", False), ("bfs", False), ("dfs", False), ("bfs", True), ("dfs", True)]
+    # rm_revs = [(None, None), ("random", False), ("bfs", False), ("dfs", False), ("bfs", True), ("dfs", True)]
+    rm_revs = [("random", False), ("bfs", False)]
 
     obj = {"nets": ns, "graphs": gs, "seeds": ss, "reorder_method_revs": rm_revs}
-    yaml_filename = "ws_dag.yaml"
+    yaml_filename = "symsa_dag.yaml"
 
     with open(yaml_filename, "w") as f:
         yaml.safe_dump(obj, f)
